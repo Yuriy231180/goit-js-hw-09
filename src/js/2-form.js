44db8aadfe1acfa5434a0c0ce2597c379aba1238
Yuriy231180 <141326084+Yuriy231180@users.noreply.github.com>
2024-01-04 23:09:42 +0200

    90401

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,41 +1,55 @@
 import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = "feedback-form-state";
-const form = document.querySelector(".feedback-form");
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
 
-const input = form.querySelector("input");
-input.classList.add("input");
+const input = form.querySelector('input');
+input.classList.add('input');
 
-const textarea = form.querySelector("textarea");
-textarea.classList.add("textarea");
+const textarea = form.querySelector('textarea');
+textarea.classList.add('textarea');
 
-const btn = form.querySelector("button");
-btn.classList.add("submitbatton");
+const btn = form.querySelector('button');
+btn.classList.add('submitbatton');
 
-const label = form.querySelector("label");
-label.classList.add("labelname");
+const label = form.querySelector('label');
+label.classList.add('labelname');
 
 const initialMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-
 if (initialMessage) {
-    textarea.value = initialMessage.message.trim();
-    input.value = initialMessage.email.trim();
-    btn.disabled = !(input.value && textarea.value)
+  textarea.value = initialMessage.message.trim();
+  input.value = initialMessage.email.trim();
+  btn.disabled = !(input.value && textarea.value);
 }
 
-form.addEventListener("input", throttle(() => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify({ email: input.value, message: textarea.value }));
+form.addEventListener(
+  'input',
+  throttle(() => {
+    localStorage.setItem(
+      STORAGE_KEY,
+      JSON.stringify({ email: input.value, message: textarea.value })
+    );
     btn.disabled = !(input.value && textarea.value);
-    
-},500))
+  }, 500)
+);
 
-form.addEventListener("submit", event => {
-    event.preventDefault();
-    console.log({ email: input.value, message: textarea.value });
-    form.reset();
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  let email = input.value.trim();
+  let message = textarea.value.trim();
+
+  if (email !== '' && message !== '') {
+    console.log({ email, message });
     localStorage.removeItem(STORAGE_KEY);
-    btn.disabled = true;
-})
+    form.reset();
+  } else {
+    alert('Будь ласка, заповніть всі поля форми.');
+  }
 
-
+  //   console.log({ email: input.value, message: textarea.value });
+  //   form.reset();
+  //   localStorage.removeItem(STORAGE_KEY);
+  btn.disabled = true;
+});
